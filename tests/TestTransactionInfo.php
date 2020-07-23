6ec7e4b3dd7b072e7cf1a5ee5766301db02ff648
DQ <hifangxcx@fujuhaofang.com>
2020-07-23 10:17:15 +0800

    UPDATE 获取身份证号码

--- tests/TestTransactionInfo.php
+++ tests/TestTransactionInfo.php
@@ -16,7 +16,7 @@ class TestTransactionInfo extends BasicTest {
     public function testGetTransactionInfo() {
         try {
             $mainLib = new Main($this->_config);
-            $return  = $mainLib->getTransactionInfo('371324199002086021');
+            $return  = $mainLib->getTransactionInfo($this->_data['card']);
             $this->assertNotEmpty($return, '提供申请资格证书信息至运营公司');
         } catch (\Exception $e) {
             $this->assertEmpty($e->getMessage(), $e->getMessage());
